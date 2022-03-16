b80e7fc6e6bb6abac9ed8fbcec1b34a2aa394cf2
Virenk21 <viren.d222111@gmail.com>
2022-03-17 03:26:53 +0530

    fetch apidata

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/ui/Header";
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className='container'>
-      <Header />
-       </div>
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchItems = async () => {
       
+      const result = await axios ('https://www.breakingbadapi.com/api/characters')
+      
+      console.log(result.data)
+      setItems(result.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, [])
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
   );
 }
 
